64ccb5fce594be969d22048682f92f8516a0d26c
Matthew Luchini <luch@Matthews-MBP.lan>
2023-02-16 15:19:53 -0500

    Static

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var celcius = TempConverter.FahrenheitToCelsius(68); //20
+            var farenheit = TempConverter.CelciusToFarenheit(20); //68
+
+            Console.WriteLine("After conversion:");
+            Console.WriteLine($"celcius {celcius}");
+            Console.WriteLine($"farenheit {farenheit}");
+            
         }
     }
 }
+
+
+
+
+//Now create a static class called TempConverter.
+//The class should have at least 2 methods, one called FahrenheitToCelsius
+//that will require a double as a parameter and return a double, the other
+//CelsiusToFahrenheit which will also require a double as a parameter and
+//return a double.
